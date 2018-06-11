b1e9b05f63bccfcad374aecb198887a779e194ac
Charles Ge <Charles.Ge@ibm.com>
2018-06-11 10:47:30 -0700

    no message

--- ambari_config.sh
+++ ambari_config.sh
@@ -64,11 +64,11 @@ echo $CLUSTER_NAME
 if [ "x$NODE_TYPE" != "xmanagement-slave00" ]
 then
     echo "Updating ambari config properties"
-    #change mapreduce.map.memory to 8192mb
-    /home/wce/clsadmin/charles/configs.py -u $AMBARI_USER -p $AMBARI_PASSWORD -s "https" -t $AMBARI_PORT -a "set" -l $AMBARI_HOST -n $CLUSTER_NAME -c "mapred-site" -k "mapreduce.map.memory.mb" -v "7192"
-    echo "Updating Custom core-site"
-    /home/wce/clsadmin/charles/configs.py -u $AMBARI_USER -p $AMBARI_PASSWORD -s "https" -t $AMBARI_PORT -a "set" -l $AMBARI_HOST -n $CLUSTER_NAME -c "core-site" -k "fs.cos.CloudObjectStorageMatt.access.key" -v "abcdefg1234567890"
-    echo "Updated Custom core-site"
+    /var/lib/ambari-server/resources/scripts/configs.py -u $AMBARI_USER -p $AMBARI_PASSWORD -s "https" -t $AMBARI_PORT -a "set" -l $AMBARI_HOST -n $CLUSTER_NAME -c "mapred-site" -k "mapreduce.map.memory.mb" -v "5192"
+    /var/lib/ambari-server/resources/scripts/configs.py -u $AMBARI_USER -p $AMBARI_PASSWORD -s "https" -t $AMBARI_PORT -a "set" -l $AMBARI_HOST -n $CLUSTER_NAME -c "core-site" -k "fs.cos.CloudObjectStorageMatt.access.key" -v "abcdefg1234567890"
+    /var/lib/ambari-server/resources/scripts/configs.py -u $AMBARI_USER -p $AMBARI_PASSWORD -s "https" -t $AMBARI_PORT -a "set" -l $AMBARI_HOST -n $CLUSTER_NAME -c "core-site" -k "fs.cos.impl" -v "aaaaa22222222"
+    /var/lib/ambari-server/resources/scripts/configs.py -u $AMBARI_USER -p $AMBARI_PASSWORD -s "https" -t $AMBARI_PORT -a "set" -l $AMBARI_HOST -n $CLUSTER_NAME -c "core-site" -k "fs.s3a.buffer.dir" -v "bbborecuroecu2333333333333"
+    /var/lib/ambari-server/resources/scripts/configs.py -u $AMBARI_USER -p $AMBARI_PASSWORD -s "https" -t $AMBARI_PORT -a "set" -l $AMBARI_HOST -n $CLUSTER_NAME -c "core-site" -k "fs.s3a.impl" -v "anocuiaro444444444444444"
     # stop MAPREDUCE2 service
     curl -v --user $AMBARI_USER:$AMBARI_PASSWORD -H "X-Requested-By: ambari" -i -X PUT -d '{"RequestInfo": {"context": "Stop MAPREDUCE2"}, "ServiceInfo": {"state": "INSTALLED"}}' https://$AMBARI_HOST:$AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/services/MAPREDUCE2
     sleep 60
